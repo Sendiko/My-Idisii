00246725a892559c3f882d8c87ac95933a2ef951
Radja Mahesa W <radjamahesaw@gmail.com>
2024-12-20 03:10:08 +0700

    Commit 2

--- app/src/main/java/com/pdt/myidisii/MainActivity.kt
+++ app/src/main/java/com/pdt/myidisii/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pdt.myidisii
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.pdt.myidisii.UI.theme.MyIdisiiTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,11 +11,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JWTAuthKtorAndroidTheme {
-                DestinationsNavHost(navGraph = NavGraphs.root)
+            MyIdisiiTheme {
+
             }
         }
     }
 }
-
 
